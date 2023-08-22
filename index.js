4cb6e11d493c16c4d73d3fecfeed7f3cc4270f6e
THE-AUDIO <nandraina.dev22@gmai.com>
2023-08-22 18:59:50 +0300

    version 3

--- index.js
+++ index.js
@@ -2,20 +2,6 @@ const injection = document.querySelector('.hidden');
 const btn = document.querySelector('.span_class');
 btn.addEventListener('click',function(){
     injection.classList.toggle('vue');
+    btn.classList.toggle('show')
+
 });
-
-const textToType = document.querySelector('h3');
-
-const typingElement = document.querySelector(".mesInfo");
-
-let charIndex = 0;
-
-function typeText() {
-  if (charIndex < textToType.length) {
-    typingElement.textContent += textToType.charAt(charIndex);
-    charIndex++;
-    setTimeout(typeText, 100);
-  }
-}
-
-typeText();
