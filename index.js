42d28ed3cf560d90de6463590c921abecb9b2970
THE-AUDIO <nandraina.dev22@gmai.com>
2023-08-23 15:45:13 +0300

    exe-depl

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const injection = document.querySelector('.hidden');
 const btn = document.querySelector('.span_class');
+const nav = document.querySelector('.nav_barre');
+const form = document.getElementById('form')
+console.log(nav)
 btn.addEventListener('click',function(){
     injection.classList.toggle('vue');
     btn.classList.toggle('show')
 
 });
+window.onscroll = function () {
+    if (window.scrollY){
+       form.classList.add('form')
+       console.log('ok');
+    }
+    else{
+        form.classList.remove('form')
+    }
+}
+window.onscroll();
